0b8cf8cc34a5d2e6dcb667566fac363cf8d22d17
Ananya <afmtrendaid@gmail.com>
2023-12-19 13:27:45 +0300

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
+
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -32,11 +35,29 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics")
+
+    // TODO: Add the dependencies for any other Firebase products you want to use
+    // See https://firebase.google.com/docs/android/setup#available-libraries
+    // For example, add the dependencies for Firebase Authentication and Cloud Firestore
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    implementation("com.google.firebase:firebase-database")
+
 }
+
+// Apply the Google Services plugin
--- app/src/main/java/com/example/alphabet/service/FirebaseService.java
+++ app/src/main/java/com/example/alphabet/service/FirebaseService.java
@@ -1,4 +1,17 @@
 package com.example.alphabet.service;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FirebaseService {
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
+    public FirebaseService() {
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    // Now you can use mAuth and mDatabase to interact with Firebase Authentication and Realtime Database.
 }
